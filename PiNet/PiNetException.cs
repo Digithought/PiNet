4c7ade4607549380f23d89b3ed6a947418290479
Adam Clayton <a.clay0x2a@gmail.com>
2015-05-07 16:51:02 -0600

    Updated naming from BlackNet to PiNet
    Updated all instances of BlackNet to PiNet in files and folders

---
+++ PiNet/PiNetException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Digithought.PiNet
+{
+	[Serializable]
+	public class PiNetException : Exception, ISerializable
+	{
+		public PiNetException(string message) : base(message)
+		{
+		}
+
+		protected PiNetException(SerializationInfo info, StreamingContext context)
+			: base(info, context)
+		{
+		}
+	}
+}
